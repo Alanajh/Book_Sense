93f16277b67d8063496422c895f0744c46cfb149
Alana Harmon <alana.harmon@yahoo.com>
2022-05-27 16:29:48 -0500

    Test

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -23,7 +23,7 @@ export default function SettingsScreen({ navigation }: RootTabScreenProps<'TabOn
         setOpen(!open);
       };
     !open && { height: 40 }
-    
+     // Testing git 
     return (
         <View style={styles.container}
         >
